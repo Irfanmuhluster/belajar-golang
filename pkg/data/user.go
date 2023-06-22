e212d70345c7b603d59b5b0a066de438d153e13d
Muhammad Irvan <muhirfan234@gmail.com>
2023-06-22 12:58:09 +0700

    DataUser public func

--- pkg/data/user.go
+++ pkg/data/user.go
@@ -4,9 +4,9 @@ import (
     "strconv"
 )
 
-func dataUser(name string, age int)(string) {
-	// name := "John"
-	// age := 25
+func DataUser() string {
+	name := "John"
+	age := 25
 
 	gabungan := "nama = " + name + ", umur = " + strconv.Itoa(age)
 	return gabungan
