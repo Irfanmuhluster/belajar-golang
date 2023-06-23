271f2b810f48acf8374f92baa86b9f267c76ee6b
Muhammad Irvan <muhirfan234@gmail.com>
2023-06-23 13:41:21 +0700

    DataUserInterface

--- pkg/data/user.go
+++ pkg/data/user.go
@@ -1,13 +1,21 @@
 package user
 
 import (
-    "strconv"
+	"strconv"
 )
 
-func DataUser() string {
-	name := "John"
-	age := 25
-
+func DataUser(name string, age int) string {
 	gabungan := "nama = " + name + ", umur = " + strconv.Itoa(age)
 	return gabungan
+}
+
+/**
+*	 type inference
+ */
+func DataUserInference() (string, string, int, float64) {
+	firstName := "Amir"
+	lastName := "Sarifudin"
+	age := 26
+	salary := 50000.25
+	return firstName, lastName, age, salary
 }
